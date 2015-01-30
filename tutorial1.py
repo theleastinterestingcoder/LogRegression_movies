88a32e70f34e1de9b37914d7880abc81aa138276
Frank Jiang <ffjiang@princeton.edu>
2015-01-30 11:13:49 -0500

    stemming and bigrams

--- tutorial1.py
+++ tutorial1.py
@@ -9,18 +9,33 @@
 '''
 import nltk
 from nltk.corpus import stopwords
+from nltk.stem.porter import *
 from collections import Counter
 import numpy as np
 import json
 from random import shuffle
 from sklearn import linear_model
 
+stemmer = PorterStemmer()
+
+def stem_tokens(tokens, stemmer):
+    stemmed = []
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
+    return stemmed
+
 def convert_to_vector(review, feature_space, feature_space_index):
     ans = np.zeros(len(feature_space_index))
 
-    for w in review.split():
+    stemmed = stem_tokens(review.split(), stemmer)
+    for w in stemmed:
         if w in feature_space:
             idx = feature_space_index[w]
+            ans[idx] = 1
+
+    for i in range(0, len(stemmed) - 2):
+        if stemmed[i] + " " + stemmed[i + 1] in feature_space:
+            idx = feature_space_index[stemmed[i] + " " + stemmed[i + 1]]
             ans[idx] = 1
 
     return ans
@@ -40,16 +55,24 @@ raw_data = json.load(open(fname, 'r'))
 
 # Collect a histogram of all the words
 fdist = Counter()
+bigramdist = Counter()
 
 
 # (1) Determine what our feature space will look like
 for page in raw_data: 
     for review in page['reviews']:
-        tmp = Counter(review[1].split())  
+        stemmed = stem_tokens(review[1].split(), stemmer)
+        tmp = Counter(stemmed)  
         fdist.update(tmp)
+        bigrams = [];
+        for i in range(0, len(stemmed) - 2):
+            bigrams.append(stemmed[i] + " " + stemmed[i + 1]);
+        tmp2 = Counter(bigrams)
+        bigramdist.update(tmp2)
 
 # We want an informative feature space of the 2000 most common words. 
 feature_space = [tuplet[0] for tuplet in fdist.most_common(2000)]
+feature_space += [tuplet[0] for tuplet in bigramdist.most_common(2000)]
 feature_space = [word.lower() for word in feature_space]
 # Filter out the most common (and probably uninformative words)
 feature_space = [word for word in feature_space if tuplet[0] not in stopwords.words('english')]
@@ -96,7 +119,7 @@ for (x,y) in zip(x_test, y_test):
 print "Accuracy is %s" % (float(correct)/len(x_test))
 
 # (5) My prediction
-review = 'This movie was terrible'
+review = 'Poor acting and a dull script make for boring viewing.'
 point = convert_to_vector(review, feature_space, feature_space_index)
 p = log_class.predict(point)[0]
 
